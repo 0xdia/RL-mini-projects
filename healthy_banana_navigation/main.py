10256b794d70b134e61c5727ed632e6140590307
Dhai Eddine Zebbiche <joseph.zebbiche@gmail.com>
2022-09-01 22:55:39 +0100

    calculate ratio of healthy banana while testing

--- healthy_banana_navigation/main.py
+++ healthy_banana_navigation/main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from monitor import run
 
 env = UnityEnvironment(file_name="./bananas_env/Banana.x86_64")
-scores = run(env, 1500, load_model=True, path='./qnetwork', save_model=True, train_mode=True)
+scores = run(env, 10, load_model=True, path='./qnetwork', save_model=False, train_mode=False)
 print(f"Best score: {max(scores)}")
 plt.plot(scores)
 plt.show()
--- healthy_banana_navigation/monitor.py
+++ healthy_banana_navigation/monitor.py
@@ -11,6 +11,7 @@ def run(env, num_episodes, load_model, path, save_model, train_mode):
     state = env_info.vector_observations[0]
     state_size = len(state)
     agent = Agent(state_size, action_size, seed, load_model, path, train_mode)
+    healthy_bananas = total_bananas = 0
     last_episode, scores = agent.episode_num, agent.scores
     for i in range(1, num_episodes+1):
         print(f"[*] Episode: {i+last_episode}")
@@ -20,17 +21,23 @@ def run(env, num_episodes, load_model, path, save_model, train_mode):
         while True:
             action = agent.act(state, 0.1)
             if not train_mode:
-                time.sleep(0.075)            
+                time.sleep(0.05)
             env_info = env.step(action)[brain_name]    
             next_state = env_info.vector_observations[0]   
             reward = env_info.rewards[0]       
             done = env_info.local_done[0]                 
             agent.step(state, action, reward, next_state, done)
             score += reward                             
-            state = next_state                     
+            state = next_state
+            if not train_mode and reward != 0:
+                total_bananas += 1
+                if reward == 1:
+                    healthy_bananas += 1
             if done:                                     
                 break
         scores.append(score)
     if train_mode and save_model:
         agent.save_model('./qnetwork', scores)
+    if not train_mode:
+        print(f"Ratio of collected healthy bananas: {healthy_bananas / total_bananas}")
     return scores
