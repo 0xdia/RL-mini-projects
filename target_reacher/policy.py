6f34919ebe1c3ae4fda874fd04fae9d87913994b
Dhai Eddine Zebbiche <joseph.zebbiche@gmail.com>
2022-09-07 18:05:59 +0100

    add policy neural net

---
+++ target_reacher/policy.py
@@ -0,0 +1,25 @@
+import torch
+
+
+class Policy(torch.nn.Module):
+    def __init__(self, state_size, action_size, hidden_layers_sizes, seed):
+        super(Policy, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.layers = torch.nn.ModuleList()
+        self.layers.append(torch.nn.Linear(state_size, hidden_layers_sizes[0]))
+        for i in range(1, len(hidden_layers_sizes)):
+            self.layers.append(torch.nn.Linear(hidden_layers_sizes[i-1], hidden_layers_sizes[i]))
+        self.layers.append(torch.nn.Linear(hidden_layers_sizes[-1], action_size))
+    
+    def forward(self, state):
+        x = state
+        for l in range(len(self.layers)-1):
+            x = torch.nn.functional.relu(self.layers[l](x))
+        return torch.nn.functional.softmax(self.layers[-1](x), dim=1)
+
+    def act(self, state):
+        state = torch.from_numpy(state).float().unsqueeze(0)
+        probs = self.forward(state)
+        m = torch.distributions.Categorical(probs)
+        action = m.sample()
+        return action.item(), m.log_prob(action)
